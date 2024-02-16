fb13d49b405e696cc6a8058b5039ac8dba1dc58f
9rrong <85331323+9rrong@users.noreply.github.com>
2024-02-17 04:49:02 +0900

    [Silver I] Title: 곱셈, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -0,0 +1,18 @@
+from sys import stdin
+
+a, b, c = map(int, stdin.readline().split())
+
+
+def dac(a, b):
+    if b == 1:
+        return a % c
+
+    tmp = dac(a, b // 2)
+
+    if b % 2 == 0:
+        return tmp * tmp % c
+    else:
+        return tmp * tmp * a % c
+
+
+print(dac(a, b))
